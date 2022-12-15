6299e8e4a114f4d793bf5b149c88c7dc0b318e9f
quangpn7 <nhutquang.mca@gmail.com>
2022-12-15 13:56:36 +0700

    Setup project App, install libs, config redux, config index.js, cdnjs load

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./assets/scss/main.scss";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/configStore";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Detail from "./pages/Detail";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Carts from "./pages/Carts";
+import Template from "./templates/HomeTemplates/Template";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={<Template />}>
+          <Route index element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/detail">
+            <Route path=":id" element={<Detail />} />
+          </Route>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/carts" element={<Carts />} />
+          <Route path="*" element={<Navigate to="" />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/templates/HomeTemplates/Template.jsx
@@ -0,0 +1,143 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const Template = () => {
+  return (
+    <div>
+      <header>
+        <div className="header__wrap">
+          <div className="header__content d-flex justify-content-between align-items-center">
+            <div className="header__logo">
+              <a href="#">
+                <img src="./img/image 3.png" alt="logoCybersoft" />
+              </a>
+            </div>
+            <div className="header__feature d-flex align-items-center">
+              <i className="fa fa-shopping-cart" />
+              <span id="item_count">(0)</span>
+              <span>
+                <a href="#" id="btnLogin">
+                  Login
+                </a>
+              </span>
+              <span>
+                <a href="./register.html" id="btnRegister">
+                  Register
+                </a>
+              </span>
+            </div>
+          </div>
+        </div>
+      </header>
+      {/* NAV */}
+      <section className="navigation">
+        <div className="navigation__wrap">
+          <ul className="nav">
+            <li className="nav-item active">
+              <a
+                className="nav-link"
+                href="#"
+                id="btnHome"
+                onclick="getDataProduct()"
+              >
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href="#"
+                id="btnMen"
+                value="men"
+                onclick="renderCategory('men')"
+              >
+                Men
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href="#"
+                id="btnWoman"
+                value="women"
+                onclick="renderCategory('women')"
+              >
+                Woman
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#" id="btnKid">
+                Kid
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#" id="btnSport">
+                Sport
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <Outlet />
+      <footer>
+        <div className="footer__wrap mx-auto">
+          <div className="row container mx-auto">
+            <div className="footer__link footer__help col-lg-4">
+              <h3>GET HELP</h3>
+              <ul className="list-unstyled">
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <a href="#">Nike</a>
+                </li>
+                <li>
+                  <a href="#">Adidas</a>
+                </li>
+                <li>
+                  <a href="#">Contact</a>
+                </li>
+              </ul>
+            </div>
+            <div className="footer__link footer__support col-lg-4">
+              <h3>SUPPORT</h3>
+              <ul className="list-unstyled">
+                <li>
+                  <a href="#">About</a>
+                </li>
+                <li>
+                  <a href="#">Contact</a>
+                </li>
+                <li>
+                  <a href="#">Help</a>
+                </li>
+                <li>
+                  <a href="#">Phone</a>
+                </li>
+              </ul>
+            </div>
+            <div className="footer__link footer__register col-lg-4">
+              <h3>REGISTER</h3>
+              <ul className="list-unstyled">
+                <li>
+                  <a href="#">Register</a>
+                </li>
+                <li>
+                  <a href="#">Login</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="footer__credit py-4">
+          <p className="text-center m-0">
+            © 2022 Cybersoft All Rights Reserved | Design Theme by Trương Tấn
+            Khải.
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Template;
